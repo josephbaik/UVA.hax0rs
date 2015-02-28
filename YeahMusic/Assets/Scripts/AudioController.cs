56af79292e822b179f554e2a405a59214fe82db3
Joseph Baik <josephsbaik@gmail.com>
2015-02-28 18:09:58 -0500

    yes

--- YeahMusic/Assets/Scripts/AudioController.cs
+++ YeahMusic/Assets/Scripts/AudioController.cs
@@ -2,14 +2,35 @@
 using System.Collections;
 
 public class AudioController : MonoBehaviour {
-
+	private AudioSource beat;
+	private AudioSource synth;
+	private AudioSource harp;
+	private AudioSource[] sources;
+	
+	public GameObject platform;
+	private GameObject[] platforms;
+	public int interval;
+	public int finterval;
+	private int start;
+	private Vector2 pos;
+	
 	// Use this for initialization
 	void Start () {
-		
+		sources = GetComponents<AudioSource>();
+		synth = sources[0];
+		harp = sources[1];
+		beat = sources[2];
+		start = 0;	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		start += interval;
+		
+		if(start >= finterval){
+			start = 0;
+			pos = new Vector2(transform.position.x + Random.Range(-10, 10), transform.position.y );
+			platform = Instantiate(platform, pos, Quaternion.identity) as GameObject;
+		}
 	}
 }
--- YeahMusic/Assets/Scripts/Platform.cs
+++ YeahMusic/Assets/Scripts/Platform.cs
@@ -4,10 +4,11 @@ using System.Collections;
 public class Platform : MonoBehaviour {
 	public int type;
 	private bool playerContact;
-	private BoxCollider hitbox;
+	private BoxCollider2D hitbox;
 	
 	// Use this for initialization
 	void Start () {
+//		type = Random.
 		switch (type){
 		case 1:
 			renderer.material.color = new Color(1,0.5f,0.5f); //C#
@@ -24,11 +25,36 @@ public class Platform : MonoBehaviour {
 		}
 		
 		playerContact = false;
-		hitbox = GetComponent<BoxCollider>();
+		hitbox = GetComponent<BoxCollider2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.Translate(new Vector2(0,-IncrementTowards(0,5,6)));
 	}
+	
+	public static float IncrementTowards (float c, float a, float t){
+		if (t == c)
+			return c;
+		else {
+			float dir = Mathf.Sign(t - c);
+			c += a * Time.deltaTime * dir;
+			return (dir == Mathf.Sign(t-c))? c: t;
+        }
+    }
+	
+	void OnCollisionEnter2D(Collision2D collision) {
+		playerContact = true;
+		MyCoroutine();
+		playerContact = false;
+	}
+	
+	IEnumerator MyCoroutine()
+	{
+		
+		
+		yield return new WaitForSeconds(2);    //Wait one frame
+		
+		
+    }
 }
