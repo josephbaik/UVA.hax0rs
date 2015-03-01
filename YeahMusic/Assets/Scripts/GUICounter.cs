9e3b463d3ced0fe7eeaf5a64eeb7eae411a1f4d7
SebastianJay <jgs3cd@virginia.edu>
2015-02-28 19:06:34 -0500

    simple gui at corner

---
+++ YeahMusic/Assets/Scripts/GUICounter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(GUIText))]
+public class GUICounter : MonoBehaviour {
+
+	public string prefix1 = "Time:";
+	public string prefix2 = "Volume:";
+
+	private float time = 0f;
+	// Use this for initialization
+	void Start () {	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		int timeSeconds = (int)time;
+
+		//todo: add in volume
+		GetComponent<GUIText> ().text = prefix1 + timeSeconds + "\n" + prefix2;
+	}
+}
